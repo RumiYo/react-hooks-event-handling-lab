63d8285474c4ab043ab521866db8d1a0e21690c7
RumiYo <yokoyamarumi@gmail.com>
2023-11-09 23:52:22 -0500

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+
+    function typingPassword(){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={typingPassword} />
+        </div>
     )
 }
 
